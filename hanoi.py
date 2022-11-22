d700ec3cb3fde310c50df4c767acbde35f609996
Wálisson Soares <wallyson2013@hotmail.com>
2022-11-22 19:37:26 -0300

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,20 @@
+def hanoi():
+    def mova(n, origem, destino, aux, jogada):
+        def mexerdisco(de, para, jogada):
+            print(f"Jogada {jogada:>3}: Torre {de} --> Torre {para}")
+            return jogada + 1 # atualiza para a próxima jogada
+
+        if n == 1:
+            jogada = mexerdisco(origem, destino, jogada)
+        else:
+            jogada = mova(n - 1, origem, aux, destino, jogada)
+            jogada = mexerdisco(origem, destino, jogada)
+            jogada = mova(n - 1, aux, destino, origem, jogada)
+
+        return jogada
+    print(" ------ Trabalho matemática Torre de hanoi ----------- ")
+    n = int(input("<@_@>-- Quantidade de discos ? -- <(@_@)> \n -- Digite aqui -->"))
+    print(f"Solução do puzzle ({2 ** n - 1} jogadas): \n")
+    mova(n, "1", "3", "2", 1)
+
+hanoi()
